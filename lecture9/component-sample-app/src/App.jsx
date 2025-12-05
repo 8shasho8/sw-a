020d4220b330c01a44db8d4271708ab25aa825a5
8shasho8 <hiroseyousuke84@gmail.com>
2025-12-05 11:51:57 +0900

    おみくじアプリの追加

--- lecture9/component-sample-app/src/App.jsx
+++ lecture9/component-sample-app/src/App.jsx
@@ -1,25 +1,12 @@
 import Header from './Header'
 import Comment from './Comment'
-
+import Omikuji from './Omikuji'
 function App() {
   return (
     <div>
       <Header firstName="Taro" lastName="Toyo" />
-      <div>
-        <Comment
-          bgColor="lightsalmon"
-          avatarUrl="https://loremflickr.com/cache/resized/65535_52947692803_fba3d9b388_q_100_80_nofilter.jpg"
-          userName="ねこ"
-          text="React頑張って!"
-        />
-        <Comment
-          bgColor="khaki"
-          avatarUrl="https://loremflickr.com/cache/resized/65535_53049747530_0c964b5139_q_100_80_g.jpg"
-          userName="Kitty"
-          text="いまどうしてる?"
-        />
+      <Omikuji />
       </div>
-    </div>
   )
 }
 
--- lecture9/component-sample-app/src/Omikuji.jsx
+++ lecture9/component-sample-app/src/Omikuji.jsx
@@ -1,13 +1,13 @@
 const Omikuji = () => {
-  const handleClick = () => {
-    const items = ['大吉', '中吉', '小吉', '吉'];
+  const handleClick = (name) => {
+    const items = ['大吉', '中吉', '小吉', '吉', '凶'];
     const index = Math.floor(Math.random() * items.length);
-    alert(items[index] + 'です！');
+    alert(name + 'さんの運勢は、' + items[index] + 'です');
   }
   return (
     <div>
       <h2>今日の運勢は？</h2>
-      <button onClick={handleClick}>おみくじを引く</button>
+      <button onClick={() => { handleClick('Toyo') }}>おみくじを引く</button>
     </div>
   );
 }
